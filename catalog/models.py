392e8b5943386d2a983d85f5bb8fdf0d87430a45
Vitaly <ivanov.arduan@yandex.ru>
2021-11-07 17:12:42 +0300

    Commit перед миграцией.

--- catalog/models.py
+++ catalog/models.py
@@ -31,9 +31,11 @@ class Book(models.Model):
     title = models.CharField(max_length=200, help_text='Введите название книги', verbose_name='Название книги')
     genre = models.ForeignKey('Genre', on_delete=models.CASCADE, help_text='Выберите жанр книги',
                               verbose_name='Жанр книги', null=True)
-    language = models.ForeignKey('Language', on_delete=models.CASCADE, help_text='Выберите язык книги', verbose_name='Язык книги', null=True)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, help_text='Выберите язык книги',
+                                 verbose_name='Язык книги', null=True)
     author = models.ManyToManyField('Author', help_text='Выберите автора книги', verbose_name='Автор книги', null=True)
-    summary = models.TextField(max_length=1000, help_text='Введите краткое описание книги', verbose_name='Анотация книги')
+    summary = models.TextField(max_length=1000, help_text='Введите краткое описание книги',
+                               verbose_name='Анотация книги')
     isbn = models.CharField(max_length=13, help_text='Должно содержать 13 символов', verbose_name='ISBN книги')
 
     def __str__(self):
@@ -43,3 +45,25 @@ class Book(models.Model):
         # Возвращает URL-адрес для доступа и определенному экземпляру книги
         return reverse('book-detail', args=[str(self.id)])
 
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, help_text='Введите статус экземпляра книги',
+                            verbose_name='Статус экземпляра книги')
+
+    def __str__(self):
+        return self.name
+
+
+class BookInstance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(max_length=20, null=True, help_text='Введите инвентарный номер экземпляра',
+                               verbose_name='Инвентарный номер')
+    imprint = models.CharField(max_length=200, help_text='Введите издательство и год выпуска',
+                               verbose_name='Издательство')
+    status = models.ForeignKey('Status', on_delete=models.CASCADE, null=True, help_text='Изменить состояние экземпляра',
+                               verbose_name='Статус экземпляра книги')
+    due_back = models.DateField(null=True, blank=True, help_text='Введите конец срока статуса',
+                                verbose_name='Дата окончания статуса')
+
+    def __str__(self):
+        return '%s %s %s' % (self.inv_nom, self.book, self.status)
