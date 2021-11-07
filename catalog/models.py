8f61048915bd73f404e2cbbb75a99767373954ef
Vitaly <ivanov.arduan@yandex.ru>
2021-11-07 13:30:45 +0300

    Формирую модели базы данных.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Введите жанр книги', verbose_name='Жанр книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20, help_text='Введите язык книги', verbose_name='Язык книги')
+
+    def __str__(self):
+        return self.name
+
