2ec1824c2b35aece5ed0838e3a765f6aff4f74f9
Vitaly <ivanov.arduan@yandex.ru>
2021-11-07 14:01:39 +0300

    Формирую модели базы данных.

--- catalog/models.py
+++ catalog/models.py
@@ -15,3 +15,14 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, help_text='Введите имя автора', verbose_name='Имя автора')
+    last_name = models.CharField(max_length=100, help_text='Введите фамилию автора', verbose_name='Фамилия автора')
+    data_of_birth = models.DateField(help_text='Введите дату рождения', verbose_name='Дата рождения', null=True,
+                                     blank=True)
+    data_of_death = models.DateField(help_text='Введите дату смерти', verbose_name='Дата смерти', null=True, blank=True)
+
+    def __str__(self):
+        return self.last_name
+
