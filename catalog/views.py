d3b09a76a4580bb179588fa7fc048fd4204426cf
Vitaly <ivanov.arduan@yandex.ru>
2021-11-10 10:17:07 +0300

    [update] Пользовательский интерфейс.

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
-    return HttpResponse('Главная стараница сайта Мир книг')
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = "На складе")
+    # Здесь метод 'all()' применен по умолчанию
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Авторы книг
+    num_authors = Author.objects.count()
+    # Отрисовка HTML-шаблона index.html  с данными внутри переменной context
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors,
+                           'num_visits': num_visits},
+                  )
