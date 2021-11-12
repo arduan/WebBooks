20cc304732e07ea1a25daff8655e1bb5083e4ca8
Vitaly <ivanov.arduan@yandex.ru>
2021-11-12 13:02:38 +0300

    Настройка форм ввода пароля и тд.

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from catalog import views
 from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
