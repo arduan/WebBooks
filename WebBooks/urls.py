d88a28c62e6254ff7343dbe1cd012c31f5dc109b
Vitaly <ivanov.arduan@yandex.ru>
2021-11-12 07:51:23 +0300

    Страница авторов. Но не отображается даты смери и рождения.

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail')
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
